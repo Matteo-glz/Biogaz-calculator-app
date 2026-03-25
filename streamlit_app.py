13ca5816523b06f2dcba47d864ec4db99db88c24
Matteo-glz <matteogalizia0407@gmail.com>
2026-03-25 14:24:25 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -94,7 +94,7 @@ if st.button("Calculer"):
     st.markdown(f"""
     <div class="result-box">
         ♻️ Quantités de déchets estimées <br><br>
-        <strong>{dechets_annuel} kg / an</strong>
+        <strong>{dechets_annuel} kg / an</strong> <br><br>
         💰 Economies estimées <br><br>
         <strong>{round(gain_total,0)} € / an</strong> <br><br>
     </div>
